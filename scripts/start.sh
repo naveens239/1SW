7630cc812fcea5583a8d444b36b82bdf1e26608e
pramodam <emailppc@gmail.com>
2014-10-29 13:14:18 +0530

    nodejs start script; execute from project folder aftert db is running.

---
+++ scripts/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo "                         *  *  *  *  *  *  *  *  *  *"
+echo "                       *                              *"
+echo " * * * * * * * * * * *     1 S t o p W e d . c o m      * * * * * * * * * * *"
+echo "                       *                              *"
+echo "                         *  *  *  *  *  *  *  *  *  *"
+node server/start.js
