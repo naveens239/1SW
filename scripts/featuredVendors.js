d802ad15cab0db427c66341f5d94e5fe56ed7ddd
pramodam <emailppc@gmail.com>
2014-11-24 19:04:21 +0530

    cleanup

--- scripts/featuredVendors.js
+++ scripts/featuredVendors.js
@@ -4,15 +4,19 @@ var monk       = require('monk'),
     collection = dbInstance.get('vendor_account'),
     _          = require('underscore');
 
-collection.find({featured:{$gt:0}}, { sort : { featured : 1 } }, function (err, accounts) {
-    console.log('----------------------------------------------------');
-    var vendor_name;
-    _.each(accounts, function (account, key) {
-        vendor_name = account.vendor_profile ? account.vendor_profile.vendor_name : "";
-        console.log(account.oauth_id + " : " + vendor_name);
+collection.find({featured: {$gt: 0}}, {
+        sort  : {featured: 1},
+        fields: {_id: false, oauth_id: true, "vendor_profile.vendor_name": true}
+    },
+    function (err, accounts) {
+        console.log('----------------------------------------------------');
+        var vendor_name;
+        _.each(accounts, function (account, key) {
+            vendor_name = account.vendor_profile ? account.vendor_profile.vendor_name : "";
+            console.log(account.oauth_id + " : " + vendor_name);
+        });
+        console.log('----------------------------------------------------');
+        console.log('number of records:' + accounts.length);
+        console.log('----------------------------------------------------');
+        process.exit(0);
     });
-    console.log('----------------------------------------------------');
-    console.log('number of records:' + accounts.length);
-    console.log('----------------------------------------------------');
-    process.exit(0);
-});
