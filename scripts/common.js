36574cc344c49fdd7ab4a3f737be8231dc25c1bf
pramodam <emailppc@gmail.com>
2014-11-25 18:19:00 +0530

    fix param bug

--- scripts/common.js
+++ scripts/common.js
@@ -1,10 +1,19 @@
 var monk       = require('monk'),
     dbconfig   = require('../server/config').dbconfig,
     dbInstance = monk(dbconfig.dburl),
-    collection = dbInstance.get('vendor_account');
+    collection = dbInstance.get('vendor_account'),
+    _          = require('underscore');
 
-function find(conditions, options, callback){
-    conditions = conditions ? conditions : {};
+function find(conditions, options, callback) {
+    if (_.isFunction(options)) {
+        callback = options;
+        options = null;
+    }
+    if (_.isFunction(conditions)) {
+        callback = conditions;
+        conditions = {};
+    }
+
     options ? collection.find(conditions, options, callback)
         : collection.find(conditions, callback);
 }
